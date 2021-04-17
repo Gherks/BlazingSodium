4bba8281b923402f1a3f90c7132a9ae4e2a042f7
Gherks <nils-joakim.andersson@hotmail.com>
2021-04-17 09:30:56 +0200

    Remove unnecessary HttpClient in main()

--- Client/Program.cs
+++ Client/Program.cs
@@ -19,17 +19,8 @@ namespace BlazingSodium.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            // OLD
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddHttpClient<EmployeeDataServiceInterface, EmployeeDataService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-            // NEW
-            builder.Services.AddHttpClient("BlazingSodium.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-            // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazingSodium.ServerAPI"));
 
             builder.Services.AddMsalAuthentication(options =>
             {
